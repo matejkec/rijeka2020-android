dce47a52ace9da16e3ac4f27305121229cc13a25
Matej Kec <matej.kec95@gmail.com>
2020-07-19 11:15:06 +0200

    .gitignore fix

--- app/src/main/java/com/rma/voicerecorder/activities/AppActivity.java
+++ app/src/main/java/com/rma/voicerecorder/activities/AppActivity.java
@@ -13,7 +13,7 @@ public class AppActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_app);
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.custom_toolbar);
